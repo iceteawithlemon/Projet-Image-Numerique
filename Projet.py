126bbc2ebe09dc580ffdb78a2e97c4a57ca3d8c8
iceteawithlemon <caitlin@lesfragnes.com>
2014-03-23 14:44:20 +0100

    ajout fonction convolution

--- Projet.py
+++ Projet.py
@@ -95,22 +95,54 @@ def degradeHorizontal(start, end):
 			dest[x][y] = int(start + x*inc)
 	return dest
 
+def sum_coef(filtre):
+	sum = 0
+	for y in filtre:
+		for x in y:
+			sum += x
+	return sum
 
+def convolutionPGM(src, filtre):
+	somme = sum_coef(filtre)
+	hauteur_filtre = len(filtre)
+	largeur_filtre = len(filtre[0])
+	dest = [[0 for x in range(0, LARGEUR)] for x in range(0, HAUTEUR)]
+	lf = int(float(largeur_filtre) / 2)
+	hf = int(float(hauteur_filtre) / 2)
+	for y in range(0, HAUTEUR - hf):
+		for x in range(0, LARGEUR - lf):
+			tmp = 0
+			for i in range(0, hauteur_filtre):
+				for j in range(0, largeur_filtre):
+					tmp += (src[x-j][y-i] * filtre[i][j])/somme
+			dest[x][y] = tmp
+	return dest
+
+
+
+
+
+
+
+filtre_moyenneur = [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
+filtre_gaussien3 = [ [1, 2, 1], [2, 4, 2], [1, 2, 1]];
+filtre_gaussien5 = [ [1, 4, 6, 4, 1], [4, 16, 25, 16, 4], [6, 24, 36, 24, 6], [4, 16, 25, 16, 4], [1, 4, 6, 4, 1]];
 
 
 
 boats = lirePGM("images/boats.pgm")
-ecrirePGM("boats_test.pgm", boats)
-#boats_test = lirePGM("boats_test.pgm")
-fleur = lirePPM("images/fleur.ppm")
-ecrirePPM("fleur_test.ppm", fleur)
-ecrirePGM("fleurBW.pgm", RGBtoGray(fleur))
-ecrirePGM("boatsBin.pgm", binarisation(boats, 128))
-colour = [128, 200, 100]
-ecrirePGM("imageUnieBW.pgm", imageUnie(128))
-ecrirePPM("imageUnieRGB.ppm", imageUnie(colour))
-ecrirePGM("degradHorizon.pgm", degradeHorizontal(1, 255))
+# ecrirePGM("boats_test.pgm", boats)
+# fleur = lirePPM("images/fleur.ppm")
+# ecrirePPM("fleur_test.ppm", fleur)
+# ecrirePGM("fleurBW.pgm", RGBtoGray(fleur))
+# ecrirePGM("boatsBin.pgm", binarisation(boats, 128))
+# colour = [128, 200, 100]
+# ecrirePGM("imageUnieBW.pgm", imageUnie(128))
+# ecrirePPM("imageUnieRGB.ppm", imageUnie(colour))
+# ecrirePGM("degradHorizon.pgm", degradeHorizontal(1, 255))
 
+ecrirePGM("test_convol_moy.pgm", convolutionPGM(boats, filtre_moyenneur))
+ecrirePGM("test_convol_gauss5.pgm", convolutionPGM(boats, filtre_gaussien5))
 
 
 
